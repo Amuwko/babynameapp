8fa5bac90f56c2fc23d32dfe9652ba6aca10128c
Amuwko <abashevmv@gmail.com>
2024-09-20 11:22:41 +0300

    Create nameDataset.js

---
+++ nameDataset.js
@@ -0,0 +1,8 @@
+// namesDataset.js
+export const namesDataset = [
+    { name: "Mike", vector: [1, 3, 1, 2, 2, 2, 2, 0, 1, 2] },
+    { name: "Ella", vector: [1, 1, 1, 3, 1, 1, 1, 1, 1, 2] },
+    { name: "Samuel", vector: [2, 2, 2, 1, 2, 2, 2, 0, 2, 2] },
+    { name: "William", vector: [2, 2, 2, 1, 2, 2, 2, 0, 2, 3] }
+    // Add more names here...
+];
